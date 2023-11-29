a24ce594700a847d86ed07a5fec3a26287f0a0b0
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-29 12:56:35 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker exec app3 php artisan view:clear
 docker exec app3 php artisan route:clear
 docker exec app3 php artisan migrate
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+docker exec app3 php artisan config:cache
+docker exec app3 php artisan route:cache
+docker exec app3 php artisan view:cache
 
